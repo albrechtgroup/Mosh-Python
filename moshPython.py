95e877179610e798c8f67250553ff2f0c8d47c2a
Andy Albrecht <aalbrecht33733@gmail.com>
2022-12-27 18:32:51 -0500

    Python 3 with Mosh

---
+++ moshPython.py
@@ -0,0 +1,41 @@
+# One Hour Python! - codeWithMosh.com
+# Printing a string:
+print("Hello World!!!")
+
+# Declare variables:
+greet = print("Hello World!!!")
+age = 33
+
+print(age) # 33
+# Changing the variable:
+age = 42.75
+print(age)
+
+first_name = "Mozart"
+
+# Boolean value's Capitalized: True or False
+onLine = True
+print(onLine) # True
+
+# Exercise:
+# We check in a patient named John Smith.
+patient = "John Smith"
+# He is 20 years old.
+age = 20
+# He is a new patient.
+newPatient = True
+
+# input() receices info from the user:
+name = input("What is your name? ")
+# Below is string 'concatination':
+print("Hello " + name)
+
+# Type Conversion:
+birth_year = input("Enter your birth year: ")
+# int() turns the string into a integer
+age = 2023 - int(birth_year)
+print(age)
+
+# float() turns the integer into a float
+my_age = 42
+print(float(my_age)) # 42.0
